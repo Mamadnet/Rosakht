960805c4a317d9440ee2f23976b96db2e8c6b27e
mamadnet <g1287327265>
2013-08-01 11:14:18 +0430

    initial kardex

---
+++ zirsakht-stock/kardex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zirsakht_stock
+{
+    public partial class kardex : zirsakht_stock.template
+    {
+        public kardex()
+        {
+            InitializeComponent();
+        }
+    }
+}
